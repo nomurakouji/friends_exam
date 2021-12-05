d337c5a3fd293c2d9bf81f515bc8e408f0fe25ee
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-05 23:13:23 +0900

    文章投稿機能

---
+++ db/migrate/20211205110059_add_content_to_feeds.rb
@@ -0,0 +1,5 @@
+class AddContentToFeeds < ActiveRecord::Migration[6.0]
+  def change
+    add_column :feeds, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_032345) do
+ActiveRecord::Schema.define(version: 2021_12_05_110059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_032345) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
   end
 
 end
