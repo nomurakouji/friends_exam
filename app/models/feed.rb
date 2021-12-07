3eb1c2fef78402d2068a29e951d40e5d183ea106
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-07 15:39:25 +0900

    投稿のバリデーション機能

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,6 @@
 class Feed < ApplicationRecord
     mount_uploader :image, ImageUploader
     belongs_to :user
+    validates :image,  presence: true
+    validates :content, presence: true, length: { maximum: 50 }
 end
