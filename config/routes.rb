4262d0d19db3f0751536902e00396321f0c3ae18
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-07 22:54:13 +0900

    route追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :feeds
   resources :sessions, only:[:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  root 'feeds#index'
 end
