040b61b86e2bcc35e76485dd5847c8d37c9a186a
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-08 14:19:50 +0900

    投稿確認機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  resources :feeds
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions, only:[:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   root 'feeds#index'
