cc1b29f2103b8dc39dbaff6f66d99960dd09fbff
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-08 16:30:59 +0900

    インデント修正

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -60,13 +60,13 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_feed
-      @feed = Feed.find(params[:id])
-    end
-    # Only allow a list of trusted parameters through.
-    def feed_params
-      params.require(:feed).permit(:content, :image, :image_cache)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_feed
+    @feed = Feed.find(params[:id])
   end
+  # Only allow a list of trusted parameters through.
+  def feed_params
+    params.require(:feed).permit(:content, :image, :image_cache)
+  end
+end
 
